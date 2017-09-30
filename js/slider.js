b7e1066513161f0dc7727fe54973a9e90a8a4acf
Unknown <phenombeatbox@gmail.com>
2017-09-30 21:20:31 +0300

    my project

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(document).on('ready', function() {
+    $(".slider").slick({
+        dots: true,
+        vertical: false,
+        centerMode: true,
+        arrows: false,
+        centerPadding: 0,
+        autoplay: true
+    });
+});
